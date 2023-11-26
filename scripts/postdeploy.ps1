3385c9d9b2952dafb7b1115502e1d1bd0283790c
Victor Vazquez <vhvb1989@gmail.com>
2023-11-26 04:00:13 +0000

    scripts

---
+++ scripts/postdeploy.ps1
@@ -0,0 +1,7 @@
+Write-Host 'Creating Python virtual environment "scripts/.venv"'
+python -m venv .\scripts\.venv
+
+Write-Host 'Installing dependencies from "requirements.txt" into virtual environment'
+.\scripts\.venv\Scripts\python -m pip install -r .\requirements.txt
+
+.\scripts\.venv\Scripts\python setup.py -s $env:AZURE_SUBSCRIPTION_ID -r $env:AZURE_DATA_INGEST_FUNC_RG -f $env:AZURE_DATA_INGEST_FUNC_NAME
