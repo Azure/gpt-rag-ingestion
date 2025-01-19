d96353685f6cb61c7139cc98f48fea88c12eb510
namtran6701 <nam3864779@gmail.com>
2025-01-18 20:53:11 -0500

    use docint exceptions for docint not available error

--- chunking/exceptions.py
+++ chunking/exceptions.py
@@ -1,3 +1,6 @@
 class UnsupportedFormatError(Exception):
     """Exception raised when a format is not supported."""
     pass
+
+class DocIntNotAvailableError(Exception):
+    "Exception raised when Document Intelligence 4.0 API is "
